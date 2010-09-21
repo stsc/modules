cb130a4c92fec9b302301558adba8d27742539f8
Steven Schubiger <schubiger@cpan.org>
2010-09-21 12:14:23 +0200

    released 0.89_01

--- DateTime-Format-Natural/trunk/lib/DateTime/Format/Natural.pm
+++ DateTime-Format-Natural/trunk/lib/DateTime/Format/Natural.pm
@@ -18,7 +18,7 @@ use Params::Validate ':all';
 use Scalar::Util qw(blessed);
 use Storable qw(dclone);
 
-our $VERSION = '0.89';
+our $VERSION = '0.89_01';
 
 validation_options(
     on_fail => sub
